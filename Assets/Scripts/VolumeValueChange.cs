62aea379350ffd570a12ec5bce57923e699088fa
Driekiann <agorin1@univ-lyon2.fr>
2020-01-06 16:03:48 +0100

    Ajout du menu, modification de l'eventsystem pour regler un bug lie aux cancel button, tentative d'un script volume son mais pas teste avec du vrai son

---
+++ Assets/Scripts/VolumeValueChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeValueChange : MonoBehaviour
+{
+    private AudioSource music;
+    private float musicVolume = 1f;
+
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        music.volume = musicVolume;
+    }
+
+    public void SetVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
