42fd6979858c6a6e24314e00cd86a7d5b215d772
GaelleMarais <marais.gaelle@gmail.com>
2020-01-07 08:52:16 +0000

    Add TMPro for Gameover text

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
-        GetComponent<Text>().text = "GAME OVER \n" + Score.value;
+        GetComponent<TextMeshProUGUI>().text = "GAME OVER \n" + Score.value;
     }
 
     // Update is called once per frame
